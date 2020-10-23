5d3df4f7cf8fb020e9109007df53ebd6fcfd7a2b
Cody Bonsma <codybonsma@gmail.com>
2020-10-22 22:56:52 -0400

    populated the current date in the Schema info

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,9 +13,14 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 })
 
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+})
+
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
         .then((foundWorkouts) => {
+            console.log("found workout informaiton: ", foundWorkouts)
             res.json(foundWorkouts);
         })
         .catch((err) => {
@@ -29,7 +34,7 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.post("/api/workouts", (req, res) => {
-    console.log(res.body)
+    console.log(req.body)
     db.Workout.create(req.body).then((newWorkout) => {
         res.json(newWorkout);
         console.log("successfully created a new workout");
@@ -49,20 +54,23 @@ router.put("/api/workouts/:id", (req, res) => {
     console.log(req.params)
     let userId = req.params.id;
     console.log("hit the update route for working with ID");
-    db.Workout.findByIdAndUpdate({_id: userId}, {
-        $push:{ exercises: {
-            type: req.body.type,
-            name: req.body.name,
-            distance: req.body.distance,
-            duration: req.body.duration,
-            weight: req.body.weight,
-            reps: req.body.reps,
-            sets: req.body.sets,
-    }}}, {new: true}, 
-    ).then((updatedWorkout) =>{
+    db.Workout.findByIdAndUpdate({ _id: userId }, {
+        $push: {
+            exercises: {
+                type: req.body.type,
+                name: req.body.name,
+                distance: req.body.distance,
+                duration: req.body.duration,
+                weight: req.body.weight,
+                reps: req.body.reps,
+                sets: req.body.sets,
+            }
+        }
+    }, { new: true },
+    ).then((updatedWorkout) => {
         console.log(updatedWorkout);
         res.json(updatedWorkout);
-    }).catch((err) =>{
+    }).catch((err) => {
         console.log(err);
         res.json({
             error: true,
@@ -70,6 +78,10 @@ router.put("/api/workouts/:id", (req, res) => {
             message: "Failed to create a new workout"
         });
     })
+})
+
+router.get("/api/workouts/range", (req,res) => {
+    console.log(req.body);
 })
 
 module.exports = router;

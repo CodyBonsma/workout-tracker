25062251a2b64e6ff8dac1d2448409aa254bafa6
Cody Bonsma <codybonsma@gmail.com>
2020-10-22 22:21:19 -0400

    succesfully added a new exercise to the workout by ID

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,23 +5,71 @@ const path = require("path");
 const db = require("../models")
 
 
-router.get("/", (req,res) =>{
+router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
 })
 
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-      .then((foundWorkouts) => {
-        res.json(foundWorkouts);
-      })
-      .catch((err) => {
+        .then((foundWorkouts) => {
+            res.json(foundWorkouts);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json({
+                error: true,
+                data: null,
+                message: "Failed to retrieve workouts.",
+            });
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    console.log(res.body)
+    db.Workout.create(req.body).then((newWorkout) => {
+        res.json(newWorkout);
+        console.log("successfully created a new workout");
+    }).catch((err) => {
         console.log(err);
         res.json({
-          error: true,
-          data: null,
-          message: "Failed to retrieve workouts.",
+            error: true,
+            data: null,
+            message: "Failed to create a new workout"
         });
-      });
-  });
+    })
+});
 
-  module.exports = router;
+// PUT route to update workout by ID
+router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body);
+    console.log(req.params)
+    let userId = req.params.id;
+    console.log("hit the update route for working with ID");
+    db.Workout.findByIdAndUpdate({_id: userId}, {
+        $push:{ exercises: {
+            type: req.body.type,
+            name: req.body.name,
+            distance: req.body.distance,
+            duration: req.body.duration,
+            weight: req.body.weight,
+            reps: req.body.reps,
+            sets: req.body.sets,
+    }}}, {new: true}, 
+    ).then((updatedWorkout) =>{
+        console.log(updatedWorkout);
+        res.json(updatedWorkout);
+    }).catch((err) =>{
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "Failed to create a new workout"
+        });
+    })
+})
+
+module.exports = router;
